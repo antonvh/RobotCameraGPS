caa0cfd82e6fa43c29a4dee6556f60615d02669a
Anton Vanhoucke <Anton@fabrique.nl>
2017-08-09 22:37:50 +0200

    Storing markers in a list with their centers.

--- test_cv.py
+++ test_cv.py
@@ -4,6 +4,10 @@ import cv2
 import numpy as np
 import time
 
+def center(contour):
+    # print(contour[0][0])
+    return (contour[0][0][0]+contour[2][0][0])/2, (contour[0][0][1]+contour[2][0][1])/2
+
 cv2.namedWindow("cam", cv2.WINDOW_OPENGL)
 cap = cv2.VideoCapture(0)
 
@@ -12,17 +16,16 @@ t = time.time()
 # Capture frames from the camera
 while True:
 
-
-    ret, img = cap.read()
-    if not ret:
-        continue
+    ok, img = cap.read()
+    if not ok:
+        continue # anyway
 
     # convert to grayscale
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # # find edges and dilate. Nice & fast!
-    # img = cv2.Canny(img, 100, 200)
-    # img = cv2.dilate(img, np.ones((6, 6)))
+    # img_grey = cv2.Canny(img_grey, 100, 200)
+    # img_grey = cv2.dilate(img_grey, np.ones((3, 3)))
 
     # OR...
 
@@ -33,15 +36,46 @@ while True:
 
     # Otsu's thresholding. Nice & fast!
     # http://docs.opencv.org/trunk/d7/d4d/tutorial_py_thresholding.html
-    values, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    values, img_grey = cv2.threshold(img_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
 
     # Find contours and tree
-    img, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    img_grey, contours, hierarchy = cv2.findContours(img_grey, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Find contours with at least x children. These must be qr markers!
+    # Find square contours with at least 2 children. These must be qr markers!
+    markers = []
+    for x in range(0, len(contours)):
+
+        k = x
+        c = 0
+
+        # Look for nested squares
+        while (hierarchy[0][k][2] != -1):
+            # As long as k has children [2], find that child and look for children again.
+            k = hierarchy[0][k][2]
+            c = c + 1
+
+        if hierarchy[0][k][2] != -1:
+            c = c + 1
+
+        if c >= 2:
+            approx = cv2.approxPolyDP(contours[x], cv2.arcLength(contours[x], True)*0.02, True)
+            # approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
+            if len(approx) == 4:
+                # print(approx)
+                markers += {'id': x,
+                            'contour': approx,
+                            'center': center(approx)
+                            }
+                print(center(approx))
+                cv2.drawContours(img, [approx], -1, (0,0,255), 3, lineType=cv2.LINE_4)
 
     # Find markers that belong together
+    for m in markers:
+        pass
+        # Calculate list of buddy positions (aligned & within distance)
+        # Check list of markers against buddy list
+        # See if we can find two other buddy markers
 
     # Calculate their positions and rotations using moments
 
