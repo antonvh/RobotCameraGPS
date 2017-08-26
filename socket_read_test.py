db53460143a1b97c3cf38d922ed6d0e55da679e9
Anton Vanhoucke <Anton@fabrique.nl>
2017-08-26 13:01:07 +0200

    Cleaning up

--- socket_read_test.py
+++ socket_read_test.py
@@ -1,31 +1,44 @@
-import socket, sys, struct, threading
+#!/usr/bin/env python3
+
+import socket, sys, struct
+from threading import Thread
 import time
+import logging
 try:
     import cPickle as pickle
 except:
     import pickle
 
-HOST = "127.0.0.1"
+
+### Initialize ###
+HOST = "127.0.0.1"  # Location server
 PORT = 50008
+RETRY_DELAY = 3     # Seconds
+THIS_ROBOT = 1      # Our own ID
 robot_positions = {}
 running = True
+logging.basicConfig(  # filename='position_server.log',     # To a file. Or not.
+    filemode='w',                                           # Start each run with a fresh log
+    format='%(asctime)s, %(levelname)s, %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.INFO, )                                   # Log info, and warning
 
 
+### Get robot positions from server ###
 def get_positions():
     global robot_positions, running
     connected = False
-    while not connected:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while not connected and running:
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             # Open socket to the location server
             s.connect((HOST, PORT))
         except:
-            print("Connection failed. Trying again in 3s...")
-            time.sleep(3)
-            continue
-        connected = True
-        # continue # Keep trying
-        print("connected")
+            logging.info("Connection to {1}:{2} failed. Trying again in {0}s...".format(RETRY_DELAY, HOST, PORT))
+            time.sleep(RETRY_DELAY)
+            continue        # Keep trying
+        connected = True    # Because we got past trying
+        logging.info("Connected to {0}:{1}".format(HOST, PORT))
 
     while running:
         try:
@@ -38,17 +51,24 @@ def get_positions():
             robot_positions = pickle.loads(data)
         except:
             e = sys.exc_info()[0]
-            print(e)
+            logging.warning(e)
             break
-    s.close()
 
-get_positions_thread = threading.Thread(target=get_positions)
+    if connected:
+        s.close()
+
+
+### Run the main loop ###
+get_positions_thread = Thread(target=get_positions)
 get_positions_thread.start()
-
 while 1:
     try:
+        # Putting this in a try statement because we need to clean up after ctrl-c
+        # Motor code goes here
         time.sleep(1)
         if len(robot_positions) > 0: print(robot_positions)
     except:
-        print("Cleaning up")
+        # Something went wrong or user aborted the script
+        logging.info("Cleaning up")
         running = False
+        break
